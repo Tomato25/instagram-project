daaed302478339f76d3b24e1153e0a7cf31592d3
Hrvoje <htomic4@gmail.com>
2023-08-22 18:04:17 +0100

    Nav bar created

---
+++ app/components/Nav.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Logo from "../../public/logo.png";
+import LogoMobile from "../../public/logoMobile.png";
+import {
+  MagnifyingGlassIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  Bars3Icon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import profielImage from "../../public/profileImg.jpg";
+
+export default function Nav() {
+  return (
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+      <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto">
+        <div className="relative hidden lg:inline-grid w-24  cursor-pointer">
+          <Image
+            src={Logo}
+            layout="fill"
+            alt="Instagram logo"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-8 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src={LogoMobile}
+            layout="fill"
+            alt="Instagram logo"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="max-w-xs">
+          <div className="mt-1 relative p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <Bars3Icon className="md:hidden cursor-pointer h-6" />
+          <div className="navBtn relative">
+            <PaperAirplaneIcon className="navBtn -rotate-45" />
+            <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-400 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <Image
+            src={profielImage}
+            className="h-10 w-10 rounded-full cursor-pointer"
+            alt="Profile Image"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
