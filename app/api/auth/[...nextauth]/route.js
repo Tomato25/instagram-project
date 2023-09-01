04515d424f32b81a1ac1b11ee5da469b9f8d3a8d
Hrvoje <htomic4@gmail.com>
2023-09-01 13:42:37 +0100

    fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next"
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider(
        {
